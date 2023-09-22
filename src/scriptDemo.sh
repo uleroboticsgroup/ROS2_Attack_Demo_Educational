339d0dec31eccd867d1b31e3f361b5a616e91767
David <77758580+Dsobh@users.noreply.github.com>
2023-09-22 17:22:38 +0200

    Update scriptDemo.sh

--- src/scriptDemo.sh
+++ src/scriptDemo.sh
@@ -4,7 +4,7 @@
 sudo iptables -A INPUT -p udp --sport 41897 -j NFQUEUE --queue-num 0
 sudo iptables -A OUTPUT -p udp --dport 7429 -j NFQUEUE --queue-num 0
 
-## Funcion que combierte hexadecimal a char ##
+## Funcion que convierte hexadecimal a char ##
 hex_to_char(){
 	echo -ne "\x$1";
 }
